fcc56b2d71078c3f601cc6403f3f0c31a7f867c3
vincentgilbert <vingilbe@students.cs.ubc.ca>
2023-10-23 18:04:47 -0700

    final

--- src/Components/api.js
+++ src/Components/api.js
@@ -4,8 +4,8 @@ export const sudoku_url = 'https://sudoku-generator1.p.rapidapi.com/sudoku/gener
 export const sudokuOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'b69de9973emsha9f2245d4db27ffp15db47jsn5c0f1751fae3',
-		'X-RapidAPI-Host': 'sudoku-generator1.p.rapidapi.com'
+		'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+		'X-RapidAPI-Host': process.env.REACT_APP_RAPID_API_HOST
 	}
 };
 
